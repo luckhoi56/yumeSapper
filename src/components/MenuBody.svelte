ad1a19e0ce3c2aa49001da6ee8a089d95f62704a
Eric Luc <luckhoi89@gmail.com>
2020-09-19 01:45:46 -0700

    cat is added

---
+++ src/components/MenuBody.svelte
@@ -0,0 +1,5 @@
+<script>
+    import MenuItem from './MenuItem.svelte';
+</script>
+
+<MenuItem></MenuItem>
--- src/routes/menu.svelte
+++ src/routes/menu.svelte
@@ -1,6 +1,7 @@
 <script>
     import HeaderBody from '../components/HeaderBody.svelte';
+    import MenuBody from '../components/MenuBody.svelte';
 </script>
 
 <HeaderBody></HeaderBody>
-
+<MenuBody></MenuBody>
